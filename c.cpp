f000da58eab159f7d1bf69278c845781f5012886
Stanislav18s <polet18stones@gmail.com>
2025-04-30 03:05:44 +0500

    ЕЩЕ одно задание

--- c.cpp
+++ c.cpp
@@ -1,13 +1,94 @@
 ﻿#include <stdio.h>
-/** Упражнение 1.15 * *.Перепишите программу преобразования 
-температур, выделив само преобразование в отдельную функцию.*/
-float Transform(float c) {
-	return c * 1.8 + 32;
-}
-int main() {
+/** **Упражнение 1.16 Перепишите main предыдущей программы так, 
+чтобы она могла печатать самую длинную строку без каких-либо ограничений на ее размер.
+.*/
 
-	for (float max = 150, min = -50, C_celsies = -50;  max > min ;C_celsies +=20,min+=20) {
-		printf("C: %5.1f\t F: %.1f\n", min, Transform(C_celsies));
+
+#define MAXLINE 1000 /* максимальный размер вводимой строки */
+
+
+int getline(char line[], int MAXLINE);
+
+void copy(char to[], char from[]);
+
+
+/* печать самой длинной строки */
+
+main()
+
+{
+
+	int len; /* длина текущей строки */
+
+	int max; /* длина максимальной из просмотренных строк */
+
+	char line[MAXLINE]; /* текущая строка */
+
+	char longest[MAXLINE]; /* самая длинная строка */
+
+
+	max = 0;
+
+	while (len = getline(line, MAXLINE)) › 0)
+
+	if (len › max) {
+
+		max = len;
+
+		copy(longest, line);
+
 	}
+
+	if (max › 0) /* была ли хоть одна строка? */
+
+		printf("%s", longest);
+
 	return 0;
+
+}
+
+
+/* getline: читает строку в s, возвращает длину */
+
+int getline(char s[], int lim)
+
+{
+
+	int c, i;
+
+
+	for (i = 0; i ‹ lim - 1 && (c = getchar()) != EOF && с != '\n'; ++i)
+
+		s[i] = c;
+
+	if (c == '\n') {
+
+		s[i] = c;
+
+		++i;
+
+	}
+
+	s[i] = '\0';
+
+	return i;
+
+}
+
+
+/* copy: копирует из 'from' в 'to'; to достаточно большой */
+
+void copy(char to[], char from[])
+
+{
+
+	int i;
+
+
+	i = 0;
+
+	while ((to[i] = from[i]) != '\0')
+
+		++i;
+
 }
