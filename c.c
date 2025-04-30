9a7144a3740efcc740f2912dbc3da3d08ec45926
Stanislav18s <polet18stones@gmail.com>
2025-05-01 02:21:27 +0500

    вроде простая программка для печати строк которые больше 80 символов

--- c.c
+++ c.c
@@ -1,64 +1,28 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-
-int mygetline(char** line, size_t* lim);
-void copy(char* to, char* from);
-
+/***Упражнение 1.17**. Напишите программу печати всех вводимых строк, содержащих более 80 символов.*/
 int main() {
-    size_t size = 50;
-    int len, max = 0;
-    char* line = malloc(size * sizeof(char));
-    char* longest = malloc(size * sizeof(char));
+	int sim = 0;
+	char array[1000];
+	int temp;
+	int i;
+	int j = 0;
+	while ((temp = getchar()) != EOF)//
+	{
+		array[sim] = temp;
+		sim++;
+	}
+	printf("vivod strok > 80 simvolov\n");
+	for (i = 0;i <= sim;i++) {
+		if (array[i] == '\n') {
+			if (i - j > 80) {
+				
+				for (int b = 0; b < i - j; b++)putchar(array[j + b]);
 
-    if (!line || !longest) {
-        printf("Ошибка выделения памяти!\n");
-        free(line);
-        free(longest);
-        return 1;
-    }
-
-    while ((len = mygetline(&line, &size)) > 0) {
-        if (len > max) {
-            max = len;
-            if (size < (size_t)max) {
-                char* temp = realloc(longest, max);
-                if (!temp) {
-                    printf("Ошибка перевыделения памяти!\n");
-                    break;
-                }
-                longest = temp;
-            }
-            copy(longest, line);
-        }
-    }
-
-    if (max > 0) printf("%s", longest);
-
-    free(line);
-    free(longest);
-    return 0;
-}
-
-int mygetline(char** s, size_t* lim) {
-    int c, i = 0;
-    while (i < *lim - 1 && (c = getchar()) != '`' && c != '\n') {
-        (*s)[i++] = c;
-        if (i >= *lim - 1) {
-            *lim *= 2;
-            char* temp = realloc(*s, *lim * sizeof(char));
-            if (!temp) {
-                printf("Ошибка перевыделения памяти!\n");
-                return -1;
-            }
-            *s = temp;
-        }
-    }
-    if (c == '\n') (*s)[i++] = c;
-    (*s)[i] = '\0';
-    return i;
-}
-
-void copy(char* to, char* from) {
-    int i = 0;
-    while ((to[i] = from[i]) != '\0') i++;
+				j = i ;
+			}
+			else { j = i ; }
+		}
+	}
+	return 0;
 }
