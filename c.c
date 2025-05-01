ea291d8bb3ef260fead8bbddd0be0d4f78e14aec
Stanislav18s <polet18stones@gmail.com>
2025-05-02 02:11:30 +0500

    очень сильно не понятно где эти флаги втыкать нужно еще было поставить второй флаг  тяжко можно было воткнуть define on off  по часа искал проблему

--- c.c
+++ c.c
@@ -1,28 +1,39 @@
 ﻿#include <stdio.h>
-#include <stdlib.h>
-/***Упражнение 1.17**. Напишите программу печати всех вводимых строк, содержащих более 80 символов.*/
-int main() {
-	int sim = 0;
-	char array[1000];
-	int temp;
-	int i;
-	int j = 0;
-	while ((temp = getchar()) != EOF)//
-	{
-		array[sim] = temp;
-		sim++;
-	}
-	printf("vivod strok > 80 simvolov\n");
-	for (i = 0;i <= sim;i++) {
-		if (array[i] == '\n') {
-			if (i - j > 80) {
-				
-				for (int b = 0; b < i - j; b++)putchar(array[j + b]);
-
-				j = i ;
+/***Упражнение 1.18**. Напишите программу, которая будет в каждой
+вводимой строке заменять стоящие подряд символы пробелов и табуляций
+на один пробел и удалять пустые строки.*/
+int main() 	
+{	
+	int temp=0;
+	char flag = 0;
+	char flag_strok = 0;
+	while ((temp = getchar()) != EOF) {
+			if (temp == '\n') {
+					if (flag_strok == 1) {
+						putchar('\n');
+						flag_strok = 0;
+					}
+				flag = 0;
 			}
-			else { j = i ; }
+		
+			else if (temp == ' ' || temp == '\t') {
+					if (flag == 1) ;
+				else {
+						if (flag_strok == 1) {
+							putchar(' ');
+							flag = 1;
+						}
+			}
+					
+			
+
 		}
+		else { putchar(temp);
+		flag_strok = 1;
+		flag = 0;
+		}
+
 	}
+
 	return 0;
 }
